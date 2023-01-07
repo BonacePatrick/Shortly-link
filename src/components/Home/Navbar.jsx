c99032630ad8022982789c78b81c6dcaafbe7a09
BonacePatrick <patrickruboneza@gmail.com>
2023-01-07 16:19:39 +0200

    add log in part

--- src/components/Home/Navbar.jsx
+++ src/components/Home/Navbar.jsx
@@ -32,11 +32,41 @@ const Navbar = () => {
                 </ul>
             </div>
             <div className="items-center space-x-6 font-bold text-grayishViolet lg:flex lg:ml-20">
-                <label className='hover:text-veryDarkViolet'>Login</label>
+
+            <label htmlFor="my-modal-6" className="hover:text-veryDarkViolet">Log in</label>
+
                 <Link to='/SignUp'className='px-6 py-2 lg:px-8 lg:py-3 font-bold text-white bg-cyan 
                 rounded-full hover:opacity-70'>Sign Up</Link>
             </div>
         </div>
+
+            {/* Put this part before </body> tag */}
+            <input type="checkbox" id="my-modal-6" className="modal-toggle" />
+            <div className="modal modal-bottom sm:modal-middle">
+            <div className="modal-box">
+                
+
+            <form>
+            <div className="relative z-0 w-full mb-6 group">
+                <input type="email" name="floating_email" id="floating_email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                <label htmlFor="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
+            </div>
+            <div className="relative z-0 w-full mb-6 group">
+                <input type="password" name="floating_password" id="floating_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                <label htmlFor="floating_password" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
+            </div>
+            
+            <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Log in</button>
+            </form>
+
+
+
+
+                <div className="modal-action">
+                <label htmlFor="my-modal-6" className="bg-slate-600 py-2 px-4 text-white border-none rounded-xl">Close</label>
+                </div>
+            </div>
+            </div>
         
     </>
   )
